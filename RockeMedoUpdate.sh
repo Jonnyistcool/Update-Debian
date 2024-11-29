97ee04e64cd3f3c3f1581c672170258801f544d2
Jonnyistcool <jonnsnlplaymann@icloud.com>
2024-11-29 16:13:03 +0100

    Update RockeMedoUpdate.sh

--- RockeMedoUpdate.sh
+++ RockeMedoUpdate.sh
@@ -10,12 +10,26 @@ highlight() {
 
 # Maskiert NGINX
 
-sudo systemctl mask nginx.serivce
+sudo systemctl stop nginx.service
+
+sleep 1
+
+sudo systemctl mask nginx.service
+
+sleep 1
+
+sudo systemctl status nginx.service
+
+sleep 1
+
+sudo systemctl disable nginx.service
 
 # Setze Skript im Fehlerfall auf Abbruch
-set -e
 
+set -e
+echo
 echo "Überprüfe, ob Zabbix Agent in /opt installiert ist..."
+echo
 
 if [ -e /etc/zabbix/zabbix_agentd.conf ]; then
     echo "Zabbix agent wird dank dem script nicht funktionieren! Bitte Timo Frings Bescheid geben."
@@ -47,6 +61,7 @@ echo
 echo
 highlight "Konfiguriere die Quellenliste..."
 echo
+
 sudo sed -i '/buster-backports/d' /etc/apt/sources.list
 echo "deb http://archive.debian.org/debian buster-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list
 
@@ -57,37 +72,50 @@ echo
 sudo apt-get update
 
 # Vollständiges System-Upgrade
+echo
 highlight "Führe System-Upgrade aus..."
+echo
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 
 # Installiere fehlende Abhängigkeiten
+echo
 highlight "Behebe Abhängigkeitsprobleme..."
+echo
 sudo apt --fix-broken install -y
 
 # Lade Systemd neu
+echo
 highlight "Systemd neu laden..."
+echo
 sudo systemctl daemon-reload
 
 # Installiere Oracle Java 21
-highlight "Installiere Oracle JDK 21..."
+echo
+highlight "Installiere Minecraft... 💀 "
+echo
 wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
 sudo dpkg -i jdk-21_linux-x64_bin.deb
 
 # Konfiguriere Java-Version
+echo
 highlight "Konfiguriere Java..."
+echo
 sudo update-java-alternatives -s /usr/lib/jvm/jdk-21.0.5-oracle-x64/
 
 # Prüfe auf fehlerhafte Dienst
+echo
 highlight "Prüfe Systemd auf fehlerhafte Dienste..."
+echo
 sudo systemctl --failed
 
 
 # Docker neustart
 sudo systemctl restart docker.service
 sudo systemctl restart docker.socket
+echo
 highlight Mowoli wird neugestartet 
-
+echo
 # X11VNC Restart + Session
 sudo systemctl restart x11vnc
 # x11vnc -ncache
@@ -96,5 +124,6 @@ sudo systemctl restart x11vnc
 echo
 echo -e "\e[32m✔️ Debian-System-Update abgeschlossen! Du hast es an dich geglaubt und es geschafft Tomedo zu Fixen! Das System wird nun neu gestartet.\e[0m"
 echo
+
 # Neustart des Systems
 sudo reboot
